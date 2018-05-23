7fb6cf6d07a3203edd1630d83dfd17b1e058f8e4
Taewii <p3rfec7@abv.bg>
2018-05-23 14:45:58 +0300

    test

---
+++ Java Basic Syntax/src/p01_Expression.java
@@ -0,0 +1,9 @@
+public class p01_Expression {
+    public static void main(String[] args) {
+
+        double result = (30 + 21) * 0.5 * (35 - 12 - 0.5);
+        double finalResult = Math.pow(result, 2);
+
+        System.out.println(finalResult);
+    }
+}
